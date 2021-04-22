d7f13422dc7a6ae1c77a9949a7a5adfd52a364b8
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-04-22 15:44:45 +0000

    data mapping

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,11 +8,19 @@ interface props {
 }
 
 const About: NextPage<props> = (props) => {
-    console.log(props);
+    // console.log(props);
 
-    return <h1 className="text-blue-200">About page </h1>;
+    // return <h1 className="text-blue-200">About page {props.data[0].title}</h1>;
+    return (
+        <>
+            {props.data.map((element) => (
+                <h2 className="text-blue-200" key={element.id}>
+                    {element.title}
+                </h2>
+            ))}
+        </>
+    );
 };
-
 About.getInitialProps = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data: Post[] = await res.json();
